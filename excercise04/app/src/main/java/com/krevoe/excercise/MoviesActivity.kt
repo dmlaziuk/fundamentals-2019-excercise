5e1d72eaa3ff92b54693f4a1c73418b3c05d5803
dmlaziuk <dm.laziuk@gmail.com>
2019-11-15 01:37:22 +0300

    Complete excercise 4

---
+++ excercise04/app/src/main/java/com/krevoe/excercise/MoviesActivity.kt
@@ -0,0 +1,31 @@
+package com.krevoe.excercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.krevoe.excercise.data.DataStorage
+
+class MoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
+
+        val movies = DataStorage.getMoviesList()
+        val adapter = MoviesAdapter(this, movies) { position ->
+            val movie = movies[position]
+            val intent = DetailsActivity.createIntent(this, movie)
+            startActivity(intent)
+        }
+
+        val list = findViewById<RecyclerView>(R.id.moviesList)
+        list.adapter = adapter
+
+        val decoration = DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL)
+        decoration.setDrawable(ContextCompat.getDrawable(this, R.color.grey)!!)
+        list.addItemDecoration(decoration)
+
+    }
+}
